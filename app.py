19d28a260625156c813aba8552e16994641402e6
mithulcb <mithulcb@gmail.com>
2023-04-24 23:29:19 +0530

    final

--- app.py
+++ app.py
@@ -9,19 +9,6 @@ import smtplib
 import yagmail
 
 
-import json
-from os import environ as env
-from urllib.parse import quote_plus, urlencode
-from authlib.integrations.flask_client import OAuth
-from dotenv import find_dotenv, load_dotenv
-
-
-
-ENV_FILE = find_dotenv()
-if ENV_FILE:
-    load_dotenv(ENV_FILE)
-
-
 mydb = mysql.connector.connect(
   host='book-1.c3lkaaypjx9u.ap-south-1.rds.amazonaws.com',
   user='admin',
@@ -33,26 +20,12 @@ mycursor = mydb.cursor(buffered=True)
 x=random.randint(1,1000)
 
 app = Flask(__name__)
-app.secret_key = env.get("APP_SECRET_KEY")
 
-oauth = OAuth(app)
 
-oauth.register(
-    "auth0",
-    client_id=env.get("AUTH0_CLIENT_ID"),
-    client_secret=env.get("AUTH0_CLIENT_SECRET"),
-    client_kwargs={
-        "scope": "openid profile email",
-    },
-    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
-)
 
 @app.route("/")
 @app.route("/home",methods = ['POST','GET'])
 def home():
-    if not session.get('login'):
-        return redirect("/login")
-    return render_template("home.html", session=session.get('username'), pretty=json.dumps(session.get('user'), indent=4))
     if not session.get('login'):
         return render_template('login.html'),401
     else:
@@ -61,36 +34,8 @@ def home():
         else :
             return home_student()
 
-@app.route("/login")
-def login():
-    return oauth.auth0.authorize_redirect(
-        redirect_uri=url_for("callback", _external=True)
-    )
 
-@app.route("/callback", methods=["GET", "POST"])
-def callback():
-    token = oauth.auth0.authorize_access_token()
-    session["username"] = token
-    session['login'] = True
-    return redirect("/home")
-
-
-@app.route("/logout")
-def logout():
-    session.clear()
-    return redirect(
-        "https://" + env.get("AUTH0_DOMAIN")
-        + "/v2/logout?"
-        + urlencode(
-            {
-                "returnTo": url_for("home", _external=True),
-                "client_id": env.get("AUTH0_CLIENT_ID"),
-            },
-            quote_via=quote_plus,
-        )
-    )
-
-@app.route("/mylogin",methods = ['GET','POST'])
+@app.route("/login",methods = ['GET','POST'])
 def mylogin():
     if request.method=='POST' :
         query = """SELECT * FROM login WHERE username = '%s'""" %(request.form['username'])
